5c5a30e8b07fba0d7dc9185b13f823187ec44ab7
MaratVagizov <109537632+MaratVagizov@users.noreply.github.com>
2022-08-05 14:37:24 +0500

    Homework-25
    Homework-25

---
+++ Homework 25.1.js
@@ -0,0 +1,85 @@
+let arr = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut au`tem",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 4,
+        "title": "et porro tempora",
+        "completed": true
+    },
+    {
+        "userId": 1,
+        "id": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 6,
+        "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 7,
+        "title": "illo expedita consequatur quia in",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 8,
+        "title": "quo adipisci enim quam ut ab",
+        "completed": true
+    },
+    {
+        "userId": 1,
+
+        "id": 9,
+        "title": "molestiae perspiciatis ipsa",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 10,
+        "title": "illo est ratione doloremque quia maiores aut",
+        "completed": true
+    },
+]
+let a_true = {
+    count: 0,
+    arr: []
+}
+
+let b_false = {
+    count: 0,
+    arr: []
+}
+
+for (let user of arr) {
+    if(user.completed === true) {
+        a_true.arr.push(user)
+        a_true.count++
+    }
+    else if(user.completed === false) {
+        b_false.arr.push(user)
+        b_false.count++
+    }
+}
+
+console.log(a_true, b_false)
